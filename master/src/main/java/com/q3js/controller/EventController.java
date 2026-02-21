0c23cc5691f44f03bd0ddb4ed8ebaab783bef58f
Luka Klacar <lukaklacar@gmail.com>
2026-02-21 18:55:07 +0100

    Add global scoreboard endpoint and related data structures

--- master/src/main/java/com/q3js/controller/EventController.java
+++ master/src/main/java/com/q3js/controller/EventController.java
@@ -2,14 +2,18 @@ package com.q3js.controller;
 
 import com.q3js.service.EventService;
 import com.q3js.service.dto.CreateEventRequest;
+import com.q3js.service.dto.ScoreboardEntryResponse;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import lombok.RequiredArgsConstructor;
 import org.jboss.logging.Logger;
+
+import java.util.List;
 
 @ApplicationScoped
 @Path("/api/events")
@@ -24,5 +28,11 @@ public class EventController {
     public void ingestEvent(CreateEventRequest event) {
         LOG.infof("Received event: %s", event);
         eventService.ingestEvent(event);
+    }
+
+    @GET
+    @Path("/scoreboard")
+    public List<ScoreboardEntryResponse> getGlobalScoreboard() {
+        return eventService.getGlobalScoreboard();
     }
 }
--- master/src/main/java/com/q3js/service/EventService.java
+++ master/src/main/java/com/q3js/service/EventService.java
@@ -1,10 +1,15 @@
 package com.q3js.service;
 
 import com.q3js.service.dto.CreateEventRequest;
+import com.q3js.service.dto.ScoreboardEntryResponse;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
+import org.jooq.Field;
+import org.jooq.impl.DSL;
+
+import java.util.List;
 
 import static com.q3js.jooq.Tables.EVENTS;
 
@@ -26,5 +31,19 @@ public class EventService {
         event.setVictimClientNum(createEventRequest.getVictim().getClientNum());
         event.setVictimName(createEventRequest.getVictim().getName());
         event.store();
+    }
+
+    public List<ScoreboardEntryResponse> getGlobalScoreboard() {
+        Field<Integer> kills = DSL.count().as("kills");
+
+        return dsl.select(EVENTS.KILLER_NAME, kills)
+                .from(EVENTS)
+                .where(EVENTS.EVENT_TYPE.equalIgnoreCase("kill"))
+                .groupBy(EVENTS.KILLER_NAME)
+                .orderBy(kills.desc(), EVENTS.KILLER_NAME.asc())
+                .fetch(record -> ScoreboardEntryResponse.builder()
+                        .playerName(record.get(EVENTS.KILLER_NAME))
+                        .kills(record.get(kills))
+                        .build());
     }
 }
