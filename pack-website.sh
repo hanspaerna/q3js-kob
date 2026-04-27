7ef4aa6ea85973a13ac69992ee146e5f0787ba32
Hans Pärna <hans.paerna@native-instruments.com>
2026-04-27 13:52:17 +0200

    add pack-website script to easily push new docker packages into forgejo

---
+++ pack-website.sh
@@ -0,0 +1,3 @@
+docker build -t q3js-website ./website --platform linux/amd64
+docker tag q3js-website:latest git.vpn.tsal.al/yanumibaal/q3js-website:latest
+docker push git.vpn.tsal.al/yanumibaal/q3js-website:latest
