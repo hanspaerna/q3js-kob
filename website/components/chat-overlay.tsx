8dda8e627fb275db679a08bffe4dafc3668beaeb
Hans Pärna <hans.paerna@native-instruments.com>
2026-06-20 02:09:37 +0200

    show multi-line messages in chat overlay of the website

--- website/components/chat-overlay.tsx
+++ website/components/chat-overlay.tsx
@@ -172,7 +172,7 @@ function ChatOverlay({ serverHost, serverPort, onClose }: ChatOverlayProps) {
                                         )}
                                     </span>
                                     <span className="text-muted-foreground">: </span>
-                                    <span>{message}</span>
+                                    <span style={{ whiteSpace: 'pre-line' }}>{message.includes('\n') ? '\n' + message : message}</span>
                                 </div>
                             );
                         })}
