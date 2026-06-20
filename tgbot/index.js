21d33982e0b904782058161c8a24f6b41b63c29e
Hans Pärna <hans.paerna@native-instruments.com>
2026-06-20 02:26:33 +0200

    make tg bot reacting to replies, add say command

--- tgbot/index.js
+++ tgbot/index.js
@@ -5,6 +5,10 @@ import { env } from './env.js';
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(env.TELEGRAM_API_TOKEN, { polling: true });
 
+await bot.setMyCommands([
+    { command: 'q3', description: 'Send a message to the Skeleton\'s Lair CPMA server' },
+]);
+
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 let lastMessageSentByConsole = "";
@@ -48,12 +52,18 @@ function connectWebSocket() {
 
 connectWebSocket();
 
+const me = await bot.getMe();
+
 bot.on('message', async (msg) => {
     const text = msg.text;
-    if (!text?.startsWith('/q3 ')) return; // ignore everything else
+
+    const isSayCommand = text?.startsWith('/q3 ');
+    const isReplyToBot = msg.reply_to_message?.from?.id === me.id;
+
+    if (!isSayCommand && !isReplyToBot) return;
 
     const fullName = [msg.from.first_name, msg.from.last_name].filter(Boolean).join(' ');
-    const message = `${fullName}: ${text.slice(4)}`; // add tg user's full name, strip "/say" prefix
+    const message = `${fullName}: ${isReplyToBot ? text : text.slice(4)}`; // add tg user's full name, strip "/say" prefix
 
     try {
         const response = await fetch(`https://${env.Q3JS_WEBSITE_HOST}/api/rcon/say`, {
